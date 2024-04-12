5c131a6e7c4fb9928b21c3bdade6a54b56bf79a1
Vivek7411 <vivnehra74@gmail.com>
2024-04-12 15:30:11 +0530

    learned loops

--- Vid_57/index.js
+++ Vid_57/index.js
@@ -1,1 +1,30 @@
 console.log("Hello JavaScript");
+
+
+let length=10;
+for (let i = 0; i < length; i++) {
+    
+    console.log("hello", i , "JS");
+}
+
+let obj={
+    nam: "vivek",
+    kam: "student",
+    hoby: "watching movies",
+    "current_age": 22
+}
+
+// console.log(obj);
+
+for (const key in obj) {
+    
+        const element = obj[key];
+        console.log(key,element);
+        
+}
+
+let namMara = "Vivek Nehra";
+
+for (const i of namMara) {
+    console.log(i);
+}
