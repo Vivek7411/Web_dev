5e0c6a57956dae174e1af29d4b4fafc78b9d74cc
Vivek7411 <vivnehra74@gmail.com>
2024-04-18 10:53:12 +0530

    learned about event handling

---
+++ Event handling/script.js
@@ -0,0 +1,10 @@
+let btn = document.querySelector("#btn");
+let div = document.querySelector("div")
+
+btn.onclick = ()=>{
+    console.log("hello");
+};
+
+div.onmouseover= ()=>{
+    console.log("you are inside div");
+};
