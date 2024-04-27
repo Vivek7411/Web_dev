051dbaaa633fe3f76e393f1cf8653d3d86b62a15
Vivek7411 <vivnehra74@gmail.com>
2024-04-27 14:58:05 +0530

    learned about string in js

--- Strings/index.js
+++ Strings/index.js
@@ -4,7 +4,14 @@ let b = `ne "hra`;
 console.log(`hi my name is ${a} ${b}`);
 
 console.log(b.toUpperCase());
-
+console.log(b);
 console.log(b.length);
 
-console.log(b.replace("ne "," ne"));
+
+console.log(b.replace("ne "," ne")); // replacine ne with space + ne
+
+b=b.toUpperCase(); // entitr string can be changed but not individual characters.
+
+console.log(b);
+
+
